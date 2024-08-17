5decf1f0406e87d5229caa6144ccc1f2ee170a61
Afeez Adesina <afeezadesinaa@gmail.com>
2024-08-17 23:06:23 +0100

    .

--- src/app/homepage/page.tsx
+++ src/app/homepage/page.tsx
@@ -1,7 +1,7 @@
-import Content from "@/src/app/ui/content";
+import Content from "@/src/app/ui/Content";
 import Footer from "@/src/app/ui/Footer";
 import Header from "../ui/Header";
-import Faq from "../ui/faq";
+import Faq from "../ui/Faq";
 
 
 export default function Homepage() {
